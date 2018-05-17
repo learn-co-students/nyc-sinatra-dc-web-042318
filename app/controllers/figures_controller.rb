05841cc23fbd1a222ec3951bcef9d5d425554470
joshpson <jpearson846@gmail.com>
2018-05-17 16:17:48 -0400

    passing all the tests

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -30,4 +30,26 @@ class FiguresController < ApplicationController
     redirect to "/figures/#{@figure.id}"
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      FigureTitle.create(figure_id: @figure.id, title_id: @title.id)
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @landmark.figure_id = @figure.id
+      @landmark.save
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
