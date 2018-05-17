b9084706c557b13ddc5f96ee337d46fb86fc069f
joshpson <jpearson846@gmail.com>
2018-05-17 11:55:20 -0400

    can add new figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,9 +5,18 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    redirect to "/figures/#{@figure.id}"
   end
 
 end
