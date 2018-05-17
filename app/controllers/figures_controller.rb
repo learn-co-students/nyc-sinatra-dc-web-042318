ddececd9ffb697a060af2ed9943a7e137454c289
rkim91 <richeekim@gmail.com>
2018-05-17 14:07:17 -0400

    almost fully working

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,7 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/new' do
+    @titles = Title.all
     erb :'figures/new'
   end
 
@@ -16,6 +17,10 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      FigureTitle.create(figure_id: @figure.id, title_id: @title.id)
+    end
     redirect to "/figures/#{@figure.id}"
   end
 
