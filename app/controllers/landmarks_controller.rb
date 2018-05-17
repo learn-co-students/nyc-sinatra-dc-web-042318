f8c99c901b0abe6fdc9e98595883cf5ceaa60b5a
eckmLJE <lucas.eckman@gmail.com>
2018-05-17 15:50:14 -0400

    done

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,4 +19,17 @@ class LandmarksController < ApplicationController
         erb :"/landmarks/show"
     end
 
+    get '/landmarks/:id/edit' do
+        @landmark = Landmark.find(params[:id])
+        erb :"/landmarks/edit"
+    end
+
+    patch '/landmarks/:id' do
+        @landmark = Landmark.find(params[:id])
+        @landmark.update(params["landmark"])
+        redirect "/landmarks/#{@landmark.id}"
+    end
+
+
+
 end
